ae036208599731144b313e7360e81edaedda4e87
zhangbaokun <zhang.baokun@etao.cn>
2017-12-19 16:48:45 +0800

    打印回执

--- src/index.js
+++ src/index.js
@@ -1,61 +1,77 @@
-import {app, BrowserWindow, ipcMain,} from 'electron';
-import axios from 'axios'
+import {app, BrowserWindow, ipcMain} from 'electron';
+import request from 'request'
 import installExtension, {VUEJS_DEVTOOLS} from 'electron-devtools-installer';
 import {enableLiveReload} from 'electron-compile';
-import {ipaddress, scan, batchprint,} from './printer'
+import {ipaddress, scan, batchprint} from './printer'
 ipcMain.on('edupdatemealorder', async (event, args) => {
-	let printerdata = await axios.post(args.URL, args.data, args.config) //1.取打印数据
-	if (printerdata.data.data.mealorder.length <= 0) {
-		return false
-	}
-	let [mealorder] = printerdata.data.data.mealorder
-	if (mealorder.printdatas.length <= 0) {
-		return false
-	}
-	batchprint(mealorder.printdatas, async function(data) {
-		args.cbdata.variables.p = {
-			id: data.id,
-			printedtimes: data.printedtimes*1 + 1,
-		}
-		console.log('args.cbdata');
-		console.log(args.cbdata);
-		await axios.post(args.URL, args.cbdata, args.config) //2.打印完成回调
-	})
+    console.log(args);
+    request({ //1.请求需要打印的数据
+        url: args.config.url,
+        method: 'POST',
+        headers: args.config.headers,
+        json: args.data
+    }, function(error, response, body) {
+        if (body.data.mealorder.length <= 0) {
+            return false
+        }
+        for (let mealorder of body.data.mealorder) { //2.遍历所有未打印的订单
+            if (mealorder.printdatas.length <= 0) {
+                return false
+            }
+            batchprint(mealorder.printdatas, function(data) { //3.每单打印
+                request({
+                    url: args.config.url,
+                    method: 'POST',
+                    headers: args.config.headers,
+                    json: Object.assign(args.cbdata, {
+                        variables: {
+                            p: {
+                                id: data.id,
+                                printedtimes: data.printedtimes * 1 + 1
+                            }
+                        }
+                    })
+                }, function(error1, response1, body1) {
+                    console.log(body1);
+                })
+            })
+        }
+    })
 })
 ipcMain.on('printer.init', (event, args) => {
-	scan(function(result) {
-		mainWindow.webContents.send('printer.init', result)
-	})
+    scan(function(result) {
+        mainWindow.webContents.send('printer.init', result)
+    })
 })
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 const isDevMode = process.execPath.match(/[\\/]electron/);
 if (isDevMode)
-	enableLiveReload();
+    enableLiveReload();
 const createWindow = async () => {
-	// Create the browser window.
-	var windowOptions = {
-		width: 1080,
-		minWidth: 680,
-		height: 840,
-		title: app.getName()
-	}
-	mainWindow = new BrowserWindow(windowOptions);
-	// and load the index.html of the app.
-	mainWindow.loadURL(`http://nbw.b.etao.cn`);
-	// Open the DevTools.
-	if (isDevMode) {
-		await installExtension(VUEJS_DEVTOOLS);
-		mainWindow.webContents.openDevTools();
-	}
-	// Emitted when the window is closed.
-	mainWindow.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
-		mainWindow = null;
-	});
+    // Create the browser window.
+    var windowOptions = {
+        width: 1080,
+        minWidth: 680,
+        height: 840,
+        title: app.getName()
+    }
+    mainWindow = new BrowserWindow(windowOptions);
+    // and load the index.html of the app.
+    mainWindow.loadURL(`http://nbw.b.etao.cn`);
+    // Open the DevTools.
+    if (isDevMode) {
+        await installExtension(VUEJS_DEVTOOLS);
+        mainWindow.webContents.openDevTools();
+    }
+    // Emitted when the window is closed.
+    mainWindow.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null;
+    });
 };
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -63,18 +79,18 @@ const createWindow = async () => {
 app.on('ready', createWindow);
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-	// On OS X it is common for applications and their menu bar
-	// to stay active until the user quits explicitly with Cmd + Q
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 app.on('activate', () => {
-	// On OS X it's common to re-create a window in the app when the
-	// dock icon is clicked and there are no other windows open.
-	if (mainWindow === null) {
-		createWindow();
-	}
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow();
+    }
 });
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
--- src/printer.js
+++ src/printer.js
@@ -3,68 +3,68 @@ const os = require('os');
 const moment = require('moment');
 const portscanner = require('portscanner')
 let printers = {}
-function batchprint(datas, cb) {
-	//检查打印机
-	for (let data of datas) {
-		let printerkey = `${data.ip}:${data.port}`
-		if (!printers[printerkey]) {
-			let sprinter = Object.create(null)
-			sprinter.ip = data.ip
-			sprinter.port = data.port
-			sprinter.client = net.createConnection(data.port / 1, data.ip)
-			sprinter.client.setKeepAlive(true, 1000 * 1)
-			sprinter.client.on('timeout', function() {
-				console.log('timeout');
-			})
-			sprinter.client.on('error', function(error) {
-				console.log(error);
-			})
-			printers[printerkey] = sprinter
-		}
-		if (printers[printerkey]['client'].destroyed) {
-			printers[printerkey]['client'] = net.createConnection(data.port / 1, data.ip)
-			printers[printerkey]['client'].setKeepAlive(true, 1000 * 1)
-		}
-	}
-	for (let data of datas) {
-		let printer = printers[`${data.ip}:${data.port}`].client
-		for (let i = 0; i < data.repetition; i++) {
-			printer.write(Buffer.from(data.data, 'base64'), 'utf8', function() {
-				cb && cb(data) //每打印一份都确认一次
-			})
-		}
-	}
+async function batchprint(datas, cb) {
+    //检查打印机
+    for (let data of datas) {
+        let printerkey = `${data.ip}:${data.port}`
+        if (!printers[printerkey]) {
+            let sprinter = Object.create(null)
+            sprinter.ip = data.ip
+            sprinter.port = data.port
+            sprinter.client = net.createConnection(data.port / 1, data.ip)
+            sprinter.client.setKeepAlive(true, 1000 * 1)
+            sprinter.client.on('timeout', function() {
+                console.log('timeout');
+            })
+            sprinter.client.on('error', function(error) {
+                console.log(error);
+            })
+            printers[printerkey] = sprinter
+        }
+        if (printers[printerkey]['client'].destroyed) {
+            printers[printerkey]['client'] = net.createConnection(data.port / 1, data.ip)
+            printers[printerkey]['client'].setKeepAlive(true, 1000 * 1)
+        }
+    }
+    for (let data of datas) {
+        let printer = printers[`${data.ip}:${data.port}`].client
+        for (let i = 0; i < data.repetition; i++) {
+            printer.write(Buffer.from(data.data, 'base64'), 'utf8', function() {
+				cb && cb(data)
+            })
+        }
+    }
 }
 function scan(cb) {
-	let localnetworks = ipaddress()
-	for (let localnetwork of localnetworks) {
-		for (let i = 1; i <= 255; i++) {
-			let arr = (localnetwork.address || '192.168.0.1').split('.');
-			arr[3] = i
-			let printer_ipaddress = arr.join('.')
-			portscanner.checkPortStatus(9100, printer_ipaddress, function(error, status) {
-				if ('open' == status) {
-					cb && cb({ip: printer_ipaddress, port: 9100, status: status})
-				}
-			})
-		}
-	}
+    let localnetworks = ipaddress()
+    for (let localnetwork of localnetworks) {
+        for (let i = 1; i <= 255; i++) {
+            let arr = (localnetwork.address || '192.168.0.1').split('.');
+            arr[3] = i
+            let printer_ipaddress = arr.join('.')
+            portscanner.checkPortStatus(9100, printer_ipaddress, function(error, status) {
+                if ('open' == status) {
+                    cb && cb({ip: printer_ipaddress, port: 9100, status: status})
+                }
+            })
+        }
+    }
 }
 function ipaddress() {
-	var networkInterfaces = os.networkInterfaces();
-	let ipaddresses = []
-	for (let prop of Object.keys(networkInterfaces)) {
-		for (let ipaddress of networkInterfaces[prop]) {
-			if (ipaddress.internal || ipaddress.family.toUpperCase() == 'IPV6') {
-				continue
-			}
-			ipaddresses.push(ipaddress)
-		}
-	}
-	return ipaddresses
+    var networkInterfaces = os.networkInterfaces();
+    let ipaddresses = []
+    for (let prop of Object.keys(networkInterfaces)) {
+        for (let ipaddress of networkInterfaces[prop]) {
+            if (ipaddress.internal || ipaddress.family.toUpperCase() == 'IPV6') {
+                continue
+            }
+            ipaddresses.push(ipaddress)
+        }
+    }
+    return ipaddresses
 };
 module.exports = {
-	ipaddress,
-	scan,
-	batchprint,
+    ipaddress,
+    scan,
+    batchprint
 };
