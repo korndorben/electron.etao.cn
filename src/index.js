580ca0900f44e49394748fd69047a89b2fb1d705
zhangbaokun <zhang.baokun@etao.cn>
2018-03-06 14:01:50 +0800

    检测打印机状态

--- src/index.js
+++ src/index.js
@@ -8,8 +8,19 @@ import request from 'request'
 import {
     ipaddress,
     scan,
-    batchprint
+    batchprint,
+    test
 } from './printer'
+ipcMain.on('printer.test', async (event, args) => { //直接推送的可打印的数据
+    console.log('args');
+    let {
+        ip,
+        port,
+        msg
+    } = args
+    console.log(args);
+    test(ip, port, msg)
+})
 let printdataqueue = {}
 ipcMain.on('printdata', async (event, args) => { //直接推送的可打印的数据
     console.log('args');
@@ -77,9 +88,10 @@ ipcMain.on('edupdatemealorder', async (event, args) => {
         })
     })
 })
-ipcMain.on('printer.init', (event, args) => {
+ipcMain.on('printer.scan', (event, args) => {
+    console.log('重新扫描网络....');
     scan(function(result) {
-        mainWindow.webContents.send('printer.init', result)
+        mainWindow.webContents.send('printer.scan', result)
     })
 })
 // Keep a global reference of the window object, if you don't, the window will
@@ -116,15 +128,15 @@ app.on('ready', async function() {
         console.log('CommandOrControl+K');
         mainWindow.toggleDevTools()
     })
-
-    globalShortcut.register('CommandOrControl+Left', () => {
-        console.log('CommandOrControl+Left');
-        mainWindow.webContents.canGoBack() && mainWindow.webContents.goBack()
-    })
-    globalShortcut.register('CommandOrControl+Right', () => {
-        console.log('CommandOrControl+Right');
-        mainWindow.webContents.canGoForward() && mainWindow.webContents.canGoForward()
-    })
+    //
+    // globalShortcut.register('CommandOrControl+Left', () => {
+    //     console.log('CommandOrControl+Left');
+    //     mainWindow.webContents.canGoBack() && mainWindow.webContents.goBack()
+    // })
+    // globalShortcut.register('CommandOrControl+Right', () => {
+    //     console.log('CommandOrControl+Right');
+    //     mainWindow.webContents.canGoForward() && mainWindow.webContents.canGoForward()
+    // })
 });
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -140,8 +152,8 @@ app.on('activate', () => {
 app.on('will-quit', () => {
     // Unregister a shortcut.
     globalShortcut.unregister('CommandOrControl+K')
-    globalShortcut.unregister('CommandOrControl+Left')
-    globalShortcut.unregister('CommandOrControl+Right')
+    // globalShortcut.unregister('CommandOrControl+Left')
+    // globalShortcut.unregister('CommandOrControl+Right')
 
     // Unregister all shortcuts.
     globalShortcut.unregisterAll()
--- src/printer.js
+++ src/printer.js
@@ -1,55 +1,112 @@
-const net = require('net');
+var net = require('net'),
+    Socket = net.Socket;
 const os = require('os');
 const moment = require('moment');
-const portscanner = require('portscanner')
+
 let printers = {}
+
+function checkprinterstatus(ip, port) {
+    let printerkey = `${ip}:${port}`
+    if (!printers[printerkey] || !printers[printerkey]['client'] || printers[printerkey]['client'].destroyed) {
+        let sprinter = Object.create(null)
+        sprinter.ip = ip
+        sprinter.port = port
+        sprinter.client = net.createConnection(port / 1, ip)
+        sprinter.client.setKeepAlive(true, 1000 * 1)
+        sprinter.client.setTimeout(1000 * 60)
+        sprinter.client.on('close', function(had_error) {
+            console.log('had_error');
+            console.log(had_error);
+            console.log('打印机连接已关闭close');
+        })
+        sprinter.client.on('connect', function() {
+            console.log('打印机连接成功connect');
+        })
+        sprinter.client.on('data', (data) => {
+            console.log('接收到打印数据');
+            console.log(data.toString());
+        });
+        sprinter.client.on('drain', function() {
+            console.log('打印机休息中drain...请打扰');
+        })
+        sprinter.client.on('end', function() {
+            console.log('打印机连接已关闭end');
+        })
+        sprinter.client.on('error', function(error) {
+            console.log('打印机连接异常error');
+            console.log(error);
+            sprinter.client.end()
+        })
+        sprinter.client.on('lookup', function(err, address, family, host) {
+            console.log('loopup事件');
+            console.log(address);
+            console.log(family);
+            console.log(host);
+        })
+        sprinter.client.on('timeout', function() {
+            console.log('打印机连接超时timeout');
+            sprinter.client.end()
+        })
+
+        printers[printerkey] = sprinter
+        return printers[printerkey]
+    }
+    return printers[printerkey]
+}
+
+async function test(ip, port, msg) {
+    let printer = checkprinterstatus(ip, port)
+    // console.log('printer.client.WritableState');
+    // for (let prop in printer.client) {
+    //     if (printer.client.hasOwnProperty(prop)) {
+    //         console.log(prop);
+    //         console.log(printer.client[prop]);
+    //     }
+    // }
+    // console.log(printer.client._writableState);
+    let buffer = []
+    buffer.push(Buffer.from([0x1b, 0x40, ])) //初始化
+    buffer.push(Buffer.from('----------\n'));
+    buffer.push(Buffer.from(`${msg}\n\n\n\n\n`));
+    buffer.push(Buffer.from('----------\n'));
+    buffer.push(Buffer.from([0x1d, 0x56, 0x00, ])); //切纸
+    printer.client.write(Buffer.concat(buffer), 'utf8', function() {
+        console.log('打印完毕');
+    })
+}
 async function batchprint(datas, cb) {
-    //检查打印机
     for (let data of datas) {
-        let printerkey = `${data.ip}:${data.port}`
-        if (!printers[printerkey]) {
-            let sprinter = Object.create(null)
-            sprinter.ip = data.ip
-            sprinter.port = data.port
-            sprinter.client = net.createConnection(data.port / 1, data.ip)
-            sprinter.client.setKeepAlive(true, 1000 * 1)
-            sprinter.client.on('timeout', function() {
-                console.log('timeout');
-            })
-            sprinter.client.on('error', function(error) {
-                console.log(error);
-            })
-            printers[printerkey] = sprinter
-        }
-        if (printers[printerkey]['client'].destroyed) {
-            printers[printerkey]['client'] = net.createConnection(data.port / 1, data.ip)
-            printers[printerkey]['client'].setKeepAlive(true, 1000 * 1)
-        }
-    }
-    for (let data of datas) {
-        let printer = printers[`${data.ip}:${data.port}`].client
+        let printer = checkprinterstatus(data.ip, data.port)
         for (let i = 0; i < data.repetition; i++) {
-            printer.write(Buffer.from(data.data, 'base64'), 'utf8', function() {
-				cb && cb(data)
+            printer.client.write(Buffer.from(data.data, 'base64'), 'utf8', function() {
+                cb && cb(data)
             })
         }
     }
 }
-function scan(cb) {
-    let localnetworks = ipaddress()
-    for (let localnetwork of localnetworks) {
-        for (let i = 1; i <= 255; i++) {
-            let arr = (localnetwork.address || '192.168.0.1').split('.');
-            arr[3] = i
-            let printer_ipaddress = arr.join('.')
-            portscanner.checkPortStatus(9100, printer_ipaddress, function(error, status) {
-                if ('open' == status) {
-                    cb && cb({ip: printer_ipaddress, port: 9100, status: status})
-                }
-            })
-        }
-    }
+
+var checkPort = function(port, host, callback) {
+    var socket = new Socket(),
+        status = null;
+    // Socket connection established, port is open
+    socket.on('connect', function() {
+        status = 'open';
+        socket.end();
+    });
+    socket.setTimeout(1500); // If no response, assume port is not listening
+    socket.on('timeout', function() {
+        status = 'closed';
+        socket.destroy();
+    });
+    socket.on('error', function(exception) {
+        status = 'closed';
+    });
+    socket.on('close', function(exception) {
+        callback(null, status, host, port);
+    });
+    socket.connect(port, host);
 }
+
 function ipaddress() {
     var networkInterfaces = os.networkInterfaces();
     let ipaddresses = []
@@ -63,8 +120,31 @@ function ipaddress() {
     }
     return ipaddresses
 };
+
+function scan(cb) {
+    console.log('重新扫描网络.....');
+    console.log(ipaddress());
+    for (let localnetwork of ipaddress()) {
+        for (let i = 1; i <= 255; i++) {
+            let arr = (localnetwork.address || '192.168.0.1').split('.');
+            arr[3] = i
+            let printer_ipaddress = arr.join('.')
+            checkPort(9100, printer_ipaddress, function(error, status, host, port) {
+                if ('open' == status) {
+                    cb && cb({
+                        ip: printer_ipaddress,
+                        port: 9100,
+                        status: status
+                    })
+                }
+            })
+        }
+    }
+}
+
 module.exports = {
     ipaddress,
     scan,
-    batchprint
+    batchprint,
+    test
 };
